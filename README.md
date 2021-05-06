320bf33ae830f80e3e40e9c0d5fe58041a47c523
yuta <yangguangben@houselai.com>
2021-05-06 18:48:16 +0800

    fix: 代码格式化

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
+<!--
+ * @Author: yuta
+ * @Date: 2021-04-23 16:45:30
+ * @LastEditTime: 2021-05-06 18:46:14
+ * @LastEditors: yuta
+-->
 # RN-Demo
+
 React-Native demo
 
-1.使用React-navigation 5.0 
+1.使用 React-navigation 5.0
 
-2.使用Recoil状态管理库 + TS
+2.使用 Recoil 状态管理库 + TS
 
-3.集成第三方Bugly SDK
+3.集成第三方 Bugly SDK
 
-4.封装原生View组件供RN层使用，并与RN交互
+4.封装原生 View 组件供 RN 层使用，并与 RN 交互
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+/*
+ * @Author: yuta
+ * @Date: 2021-04-25 17:18:58
+ * @LastEditTime: 2021-05-06 18:45:54
+ * @LastEditors: yuta
+ */
 /**
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
 
 AppRegistry.registerComponent(appName, () => App);
